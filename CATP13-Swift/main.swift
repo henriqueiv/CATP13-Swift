cf6883fec33e957bb11b383b9397d1e3fea10cc6
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-10-10 14:52:14 -0300

    Inicial

--- CATP13-Swift/main.swift
+++ CATP13-Swift/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
 
+// - MARK: 2.a
+print("---------- Item 2.a ----------")
+let pessoaPadrao = Pessoa()
+let pessoaCustom = Pessoa(nome: "Henrique Valcanaia", nascimento: Date(timeIntervalSince1970: 1), sexo: .masculino)
+
+print(pessoaPadrao.description)
+print(pessoaCustom.description)
+
+// - MARK: 2.b
+print("---------- Item 2.b ----------")
+let p1 = Pessoa()
+let p2 = p1
+
+let p3 = p1.copy() as! Pessoa // necessário pois usa Any que é uma estrutura semelhante ao Object
+
+p1.setNome(nome: "Nome alterado")
+print(p1.description)
+print(p2.description)
+print(p3.description)
+
+// - MARK: 2.c
+print("---------- Item 2.c ----------")
+let a1 = Aluno()
+print(a1.description)
