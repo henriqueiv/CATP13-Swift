4180b4b0a7ee783b6d8b5a98db380bd44ea5eb02
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-10-10 13:40:28 -0300

    Initial Commit

---
+++ CATP13-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CATP13-Swift
+//
+//  Created by Henrique Valcanaia on 10/10/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
